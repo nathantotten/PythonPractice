d127dde1eccf6155f8cc71fd8f6958ac11b886b9
Nathan Totten <nathan.james.totten@gmail.com>
2021-07-03 17:32:27 +0100

    Some more basic python practice

--- Basics/test.py
+++ Basics/test.py
@@ -1,1 +1,21 @@
-print("Hello")
+import random
+
+
+def testFunc():
+    global x
+    x = "Python!"
+
+
+# testFunc()
+# print(x)
+
+# For a specified number, prints that number of random values between 1 and 100
+def randomNumberPrinter(length):
+    count = 0
+    for i in range(length):
+        count += 1
+        i = random.randrange(0, 100)
+        print("Random Number", str(count) + ":", str(i))
+
+
+randomNumberPrinter(100)
